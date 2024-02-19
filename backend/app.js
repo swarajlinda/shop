20e4197fbcf512263b8653220a8b8947370c20f1
swarajlinda <frameworkfuturistic@gmail.com>
2024-02-19 17:13:54 +0530

    corschanges

--- backend/app.js
+++ backend/app.js
@@ -24,11 +24,11 @@ dotenv.config({
 // // Using middleware
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({
-    origin:[process.env.FRONTEND_URL],
-    methods:["GET","PUT","POST","DELETE"],
-    credentials: true
-}))
+// app.use(cors({
+//     origin:[process.env.FRONTEND_URL],
+//     methods:["GET","PUT","POST","DELETE"],
+//     credentials: true
+// }))
 
 // // Routes for user
 app.use('/api/v1/users', userRoutes);
